13ce95f0b664f913e6b0d9ae0a4d1affecf0b015
kcavs <kcav1321@gmail.com>
2024-03-18 14:45:48 -0400

    yum

--- Sleep.java
+++ Sleep.java
@@ -8,7 +8,6 @@ public class Sleep {
         int dayN;
         int yearN;
         int monthN;
-        int dayN;
         int days_alive;
         int hrs_slept;
         Scanner input= new Scanner(System.in);
@@ -18,7 +17,7 @@ public class Sleep {
         System.out.print("\nmonth: ");
         monthB=input.nextInt();
         System.out.print("\nday: ");
-        dayB=input.nexrInt();
+        dayB=input.nextInt();
         System.out.println("enter todays date: ");
         System.out.print("year: ");
         yearN=input.nextInt();
@@ -26,8 +25,8 @@ public class Sleep {
         monthN=input.nextInt();
         System.out.print("\nday: ");
         dayN=input.nextInt();
-        days_alive=day+((month-1)*30)+((year-1)*365);
-        hrs_slept=(days_alive*24)-(days_alive*8);
+        days_alive=((yearN*365)+(monthN*30)+dayN)-((yearB*365)+(monthB*30)+dayB);
+        hrs_slept=((days_alive*24)-(days_alive*8))/2;
         System.out.println("you have been asleep for "+hrs_slept+" hours");
         System.out.println("you have been alive for "+days_alive+" days");
 
